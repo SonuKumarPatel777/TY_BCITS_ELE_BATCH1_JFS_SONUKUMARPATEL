334a50350ec8a6e474a5893ff12b6e4ee71ff520
Sonu kumar Patel <sonukumarpatel2606@gmail.com>
2019-12-20 16:12:05 +0530

    push to git

---
+++ searchingAlgorithms/BinarySearchAnalysis.java
@@ -0,0 +1,11 @@
+package com.ty.bcits.searchingAlgorithms;
+
+public class BinarySearchAnalysis {
+
+	public static void main(String[] args) {
+          int [] a= {4,5,6,7,8,9,11,13,14,16,18};
+         System.out.println(BinarySearchAlgorithms.binarySearch(a, 5));
+          BinarySearchAlgorithms.countDurationOfBinarySearch(a, 11);
+	}
+
+}
