a97af1eb0d26f0b21aabb34bb62a8b683e7d75c3
Sonu kumar Patel <sonukumarpatel2606@gmail.com>
2020-01-07 19:25:29 +0530

    commit-2

--- web technology/BCITS-JavaScript/Form-validation/form.js
+++ web technology/BCITS-JavaScript/Form-validation/form.js
@@ -2,10 +2,21 @@ function validateForm() {
     var loginForm = document.forms['loginForm']
     var userMail = loginForm['userEmail'].value
     var password = loginForm['userPwd'].value
-    console.log(loginForm);
-    console.log('User Mail',userMail);
-    console.log('User Password',password);
-    
-    
-    
+    // console.log(loginForm);
+    // console.log('User Mail',userMail);
+    // console.log('User Password',password);
+    if(userMail.trim().length>10) {
+        document.getElementById('emailErr').style.display='none'
+        console.log('User mail is valid');   
+    }else{
+        document.getElementById('emailErr').style.display='block'
+        console.log("User mail is invalid");    
+    }
+    if(password.trim().length>5) {
+        document.getElementById('pwdErr').style.display='none'
+        console.log('User Password is valid');   
+    }else{
+        document.getElementById('pwdErr').style.display='block'
+        console.log("User Password is invalid");   
+    }      
 }
